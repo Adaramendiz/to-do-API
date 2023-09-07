d3b756a2c3cfe8ff41c22b08ddd50116b38a68d1
andres <adaramendiz@gmail.com>
2023-09-07 16:33:22 -0500

    update

--- src/utils/database.mjs
+++ src/utils/database.mjs
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
-
-const db =  new Sequelize({
-  host: 'localhost',
-  username: 'postgres',
-  database: 'todos_db',
+const db = new Sequelize({
+  host: "localhost",
+  username: "postgres",
+  database: "todos_db",
   port: 5432,
-  password: 'root',
-  dialect: 'postgres'
+  password: "root",
+  dialect: "postgres",
+  dialectOptions: { ssl: { required: true, rejectUnauthorized: false } },
 });
 
 export default db;
